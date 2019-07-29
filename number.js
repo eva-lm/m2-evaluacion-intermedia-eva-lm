c5c25cc099627c019e4e98997602f4d27ade9c01
eva-lm <eva.lm1810@gmail.com>
2019-07-29 16:29:56 +0200

    add js

--- number.js
+++ number.js
@@ -1,9 +1,21 @@
 "use strict";
 const button = document.querySelector(".js-button");
 
-const randomNumber = function(ev) {
-    ev.preventDefault(); 
-    const select = document.querySelector(".js-select");
+const functionNumber = function(ev) {
+  ev.preventDefault();
+  const input = document.querySelector(".js-input");
+  const generateNumber = input.value;
+  input.innerHTML = generateNumber;
 
+  const randomNumber = Math.random() * 100;
+  const number = randomNumber.toFixed(0);
 
-button.addEventListener("click", randomNumber);
+  if (number == 2) {
+    const text = document.querySelector(".js-text");
+    text.innerHTML = "Holaaa";
+  } else {
+  }
+};
+
+const text = document.querySelector(".js-text");
+button.addEventListener("click", functionNumber);
